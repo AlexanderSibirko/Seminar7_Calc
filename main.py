f74d89a5edba2fc7194031db087c42fe46440c4f
Alexander <Alexandersibirko@yandex.ru>
2022-02-13 00:49:39 +0300

    commit

--- main.py
+++ main.py
@@ -2,23 +2,9 @@ from c_calc import Calc_block as c_calc
 # import console_ui
 from logger import result_logger as write_log
 
-
-# a = 6
-# op = '-'
-# b = 50
-# res = c_calc(a,op,b)
-# write_log(a,op,b,res)
-
-
-# a = 6 + 2j
+a, op, b = console_ui.ask_user()
+# a = float('5')
 # op = '+'
-# b = 50
-# res = c_calc(a,op,b)
-# write_log(a,op,b,res)
-
-
-# a = 65+ 2j
-# op = '/'
-# b = 0
-# res = c_calc(a,op,b)
-# write_log(a,op,b,res)
+# b = 3
+res = c_calc(a,op,b)
+write_log(a,op,b,res)
