e60ade76fbc52cfdf1aa612d8f9b95785eba7f72
Alexander <Alexandersibirko@yandex.ru>
2022-02-12 12:27:09 +0300

    wip

---
+++ main.py
@@ -0,0 +1,24 @@
+from c_calc import Calc_block as c_calc
+# import console_ui
+from logger import result_logger as write_log
+
+
+# a = 6
+# op = '-'
+# b = 50
+# res = c_calc(a,op,b)
+# write_log(a,op,b,res)
+
+
+# a = 6 + 2j
+# op = '+'
+# b = 50
+# res = c_calc(a,op,b)
+# write_log(a,op,b,res)
+
+
+# a = 65+ 2j
+# op = '/'
+# b = 0
+# res = c_calc(a,op,b)
+# write_log(a,op,b,res)
