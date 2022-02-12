b7846b33e4c30819ea971724d6a5cec6b20d76c0
MVCH812 <mvch812@gmail.com>
2022-02-12 17:27:45 +0300

    new solution

--- c_calc.py
+++ c_calc.py
@@ -1,30 +1,33 @@
-# модуль для расчетов операций с комплексными числами
+# модуль для расчетов операций
 
 import cmath
+from fractions import Fraction
+from re import U
 
-def Calc_block(left_value, oper, right_value):
+def Calc_block(data):
+    left_value, oper, right_value = data
     if oper == '+':
-        res = sum(left_value, right_value)
+        result = sum(left_value, right_value)
     if oper == '-':
-        res = sub(left_value, right_value)
+        result = sub(left_value, right_value)
     if oper == '*':
-        res = mult(left_value, right_value)
-    if oper =='/':
-        res = div(left_value, right_value)
-    return res
+        result = mult(left_value, right_value)
+    if oper == '/':
+        result = div(left_value, right_value)
+    return result
 
 
-def sum(left_value: complex, right_value: complex):
+def sum(left_value, right_value):
     return left_value + right_value
 
 
-def sub(left_value: complex, right_value: complex):
+def sub(left_value, right_value):
     return left_value - right_value
 
 
-def mult(left_value: complex, right_value: complex):
+def mult(left_value, right_value):
     return left_value * right_value
 
 
-def div(left_value: complex, right_value: complex):
+def div(left_value, right_value):
     return left_value / right_value
