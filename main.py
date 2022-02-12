cb872407caa357c869403ff778917c3226b0da07
Dzheb <Dzheb2009@gmail.com>
2022-02-12 19:44:57 +0300

    last

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-# import math
-import cmath
-import logger as log
-
-# a = complex(2,4)
-# b = complex(1,3)
-a = 4
-b = 7
-data = a + b
-data_str = f'{a} + {b}'
-log.oper_logger(a,'+',b,data)
-# log.res_logger(data)
-=======
 from c_calc import Calc_block as c_calc
 # import console_ui
 from logger import result_logger as write_log
@@ -36,4 +22,3 @@ from logger import result_logger as write_log
 # b = 0
 # res = c_calc(a,op,b)
 # write_log(a,op,b,res)
->>>>>>> logger
