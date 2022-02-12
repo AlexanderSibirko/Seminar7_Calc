a0ad474410123ff1ffb413812c0153bf4f44bb95
Dzheb <Dzheb2009@gmail.com>
2022-02-12 19:35:34 +0300

    temp

--- logger.py
+++ logger.py
@@ -2,12 +2,18 @@ from datetime import datetime as dt
 from time import time
 
 
-def result_logger(left_value, oper, right_value,data):
+def oper_logger(left_value, oper, right_value):
   data_str = f'{str(left_value)} {oper} {str(right_value)}'
   time = dt.now().strftime('%H:%M')
-  print('{}; операция : {} результат : {}\n'.format(time, data_str, data))
+  print('{}; операция : {} \n'.format(time, data_str))
   with open('log.csv', 'a', encoding = 'UTF-8') as file:
-    file.write('{}; операция : {} результат :{}\n'.format(time, data_str, data))
+    file.write('{}; операция : {} \n'.format(time, data_str))
+# 
+def res_logger(data):
+  time = dt.now().strftime('%H:%M')
+  print('{}; результат : {}\n'.format(time, data))
+  with open('log.csv', 'a', encoding = 'UTF-8') as file:
+    file.write('{}; результат :{}\n'.format(time, data))
     
  
 
---
+++ main.py
@@ -0,0 +1,12 @@
+# import math
+import cmath
+import logger as log
+
+# a = complex(2,4)
+# b = complex(1,3)
+a = 4
+b = 7
+data = a + b
+data_str = f'{a} + {b}'
+log.oper_logger(a,'+',b,data)
+# log.res_logger(data)
