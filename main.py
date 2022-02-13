092ffb181c9dcac5a17d1fc4e456b285ffcea43a
Dzheb <Dzheb2009@gmail.com>
2022-02-13 23:47:02 +0300

    finished

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-from c_calc import Calc_block as c_calc
-# import console_ui
-from logger import result_logger as write_log
+import controller as c
 
-
-# a = 6
-# op = '-'
-# b = 50
-# res = c_calc(a,op,b)
-# write_log(a,op,b,res)
-
-
-# a = 6 + 2j
-# op = '+'
-# b = 50
-# res = c_calc(a,op,b)
-# write_log(a,op,b,res)
-
-
-# a = 65+ 2j
-# op = '/'
-# b = 0
-# res = c_calc(a,op,b)
-# write_log(a,op,b,res)
+c.button_click()
