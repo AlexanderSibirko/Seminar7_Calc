143624ebc7b96fb89b250382598fa23b6981ad28
Dzheb <Dzheb2009@gmail.com>
2022-02-14 00:03:20 +0300

    finished

--- main.py
+++ main.py
@@ -1,26 +1,3 @@
 import controller as c
 
-<<<<<<< HEAD
-
-# a = 6
-# op = '-'
-# b = 50
-# res = c_calc(a,op,b)
-# write_log(a,op,b,res)
-
-
-# a = 6 + 2j
-# op = '+'
-# b = 50
-# res = c_calc(a,op,b)
-# write_log(a,op,b,res)
-
-
-# a = 65+ 2j
-# op = '/'
-# b = 0
-# res = c_calc(a,op,b)
-# write_log(a,op,b,res)
-=======
 c.button_click()
->>>>>>> logger
