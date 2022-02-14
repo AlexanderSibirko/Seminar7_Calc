eb270aed40dea25da09e20eb44f8cabefd05dc02
Dzheb <Dzheb2009@gmail.com>
2022-02-14 09:43:37 +0300

    finish

--- logger.py
+++ logger.py
@@ -3,18 +3,10 @@ from datetime import datetime as dt
 from time import time
 
 
-def oper_logger(left_value, oper, right_value):
+def result_logger(left_value, oper, right_value,data):
   data_str = f'{str(left_value)} {oper} {str(right_value)}'
   time = dt.now().strftime('%H:%M')
   # 
   with open('log.csv', 'a', encoding = 'UTF-8') as file:
-    file.write('{}; операция : {} \n'.format(time, data_str))
-# 
-def res_logger(data):
-  time = dt.now().strftime('%H:%M')
-  print('{}; результат : {}\n'.format(time, data))
-  with open('log.csv', 'a', encoding = 'UTF-8') as file:
-    file.write('{}; результат :{}\n'.format(time, data))
-    
+    file.write('{}; операция : {} результат :{}\n'.format(time, data_str, data))
  
-
